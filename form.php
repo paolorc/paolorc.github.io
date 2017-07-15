2acf083ac2467f322fd269fcdd1d896fbb70a5b4
paolorc <paolo.reyes.2911@gmail.com>
2017-07-15 13:49:09 -0500

    Add files via upload

--- form.php
+++ form.php
@@ -42,7 +42,7 @@
 		}
 
 // If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+if (!$errName && !$errEmail && !$errMessage) {
 	if (mail ($to, $subject, $body, $from)) {
 		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
 	} else {
