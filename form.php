f7422e0ee5d09fce685fd850ef59c4e9b5f534e4
paolorc <paolo.reyes.2911@gmail.com>
2017-08-20 21:15:31 -0500

    Update form.php

--- form.php
+++ form.php
@@ -1,53 +1,26 @@
 <?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['f_name'];
-		$email = $_POST['f_email_from'];
-		$subject = $_POST['f_subject'];
-		$message = $_POST['f_content'];
-		$prod_1 = $_POST['prod_1_amount'];
-		$prod_2 = $_POST['prod_2_amount'];
-		$prod_3 = $_POST['prod_3_amount'];
-		$prod_4 = $_POST['prod_4_amount'];
-		$prod_5 = $_POST['prod_5_amount'];
-		$prod_6 = $_POST['prod_6_amount'];
-		$from = $email; 
-		$to = 'm.vallejos.a@hotmail.com';
-		
-		$body = "Cliente: $name\n  Consulta:\n $message";
-
-		if ($prod_1 != 0 || $prod_2 != 0 || $prod_3 != 0 || $prod_4 != 0 || $prod_5 != 0 || $prod_6 != 0) {
-
-				$body = "$body\n Cotizacion incluida:\n
-						$prod_1 x Natural Gloss\n
-						$prod_2 x Odor Eliminator\n
-						$prod_3 x Carwash\n
-						$prod_4 x Heavy Spot Cleaner\n
-						$prod_5 x Tire Shine\n
-						$prod_6 x Paños de microfibra";
-		}
-
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Ingrese un nombre';
-		}
-		
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Ingrese una direccion de e-mail v&aacute;';
-		}
-		
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Ingrese su consulta';
-		}
-
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-	}
-}
-	}
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['subject'])	||
+   empty($_POST['message'])
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+	
+// Create the email and send the message
+$to = 'contacto@goclean-peru.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Contacto via web:  $subject";
+$email_body = "Acabas de recibir un mensaje de el formulario de contacto de tu pagina web.\n\n"."Con los siguientes detalles:\n\nNombre: $name\n\nCorreo: $email_address\n\nMensaje:\n$message";
+$headers = "De: noreply@geodinamika.pe\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,utf8_decode($email_body),$headers);
+return true;         
 ?>
